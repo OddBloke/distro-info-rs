2ef0523f5899c75935080b42b85b7d6163d92046
Daniel Watkins <daniel.watkins@canonical.com>
2018-06-21 17:33:31 -0400

    Add DistroRelease struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
+extern crate chrono;
+
+use chrono::naive::NaiveDate;
+
+pub struct DistroRelease {
+    pub version: String,
+    pub codename: String,
+    pub series: String,
+    pub created: Option<NaiveDate>,
+    pub release: Option<NaiveDate>,
+    pub eol: Option<NaiveDate>,
+    pub eol_server: Option<NaiveDate>,
+}
+
 #[cfg(test)]
 mod tests {
+    use chrono::naive::NaiveDate;
+    use DistroRelease;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create_struct() {
+        DistroRelease {
+            version: "version".to_string(),
+            codename: "codename".to_string(),
+            series: "series".to_string(),
+            created: Some(NaiveDate::from_ymd(2018, 6, 14)),
+            release: Some(NaiveDate::from_ymd(2018, 6, 14)),
+            eol: Some(NaiveDate::from_ymd(2018, 6, 14)),
+            eol_server: Some(NaiveDate::from_ymd(2018, 6, 14)),
+        };
+        ()
     }
 }
