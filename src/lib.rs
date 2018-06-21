68da22b0e4f1ff1285439576bb3a4b5481dfb63f
Daniel Watkins <daniel.watkins@canonical.com>
2018-06-21 17:45:30 -0400

    Add DistroRelease::new()

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,27 @@ pub struct DistroRelease {
     pub eol_server: Option<NaiveDate>,
 }
 
+impl DistroRelease {
+    pub fn new(version: String,
+               codename: String,
+               series: String,
+               created: Option<NaiveDate>,
+               release: Option<NaiveDate>,
+               eol: Option<NaiveDate>,
+               eol_server: Option<NaiveDate>)
+               -> DistroRelease {
+        DistroRelease {
+            version: version,
+            codename: codename,
+            series: series,
+            created: created,
+            release: release,
+            eol: eol,
+            eol_server: eol_server,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use chrono::naive::NaiveDate;
@@ -29,5 +50,31 @@ mod tests {
             eol_server: Some(NaiveDate::from_ymd(2018, 6, 14)),
         };
         ()
+    }
+
+    #[test]
+    fn distro_release_new() {
+        let get_date = |mut n| {
+            let mut date = NaiveDate::from_ymd(2018, 6, 14);
+            while n > 0 {
+                date = date.succ();
+                n = n - 1;
+            }
+            date
+        };
+        let distro_release = DistroRelease::new("version".to_string(),
+                                                "codename".to_string(),
+                                                "series".to_string(),
+                                                Some(get_date(0)),
+                                                Some(get_date(1)),
+                                                Some(get_date(2)),
+                                                Some(get_date(3)));
+        assert_eq!("version", distro_release.version);
+        assert_eq!("codename", distro_release.codename);
+        assert_eq!("series", distro_release.series);
+        assert_eq!(Some(get_date(0)), distro_release.created);
+        assert_eq!(Some(get_date(1)), distro_release.release);
+        assert_eq!(Some(get_date(2)), distro_release.eol);
+        assert_eq!(Some(get_date(3)), distro_release.eol_server);
     }
 }
