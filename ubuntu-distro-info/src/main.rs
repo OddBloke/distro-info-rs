5f75488f7531e85b11ed85f50f092561f0b5654b
Daniel Watkins <daniel.watkins@canonical.com>
2018-06-22 16:05:58 -0400

    [ubuntu-distro-info] --all implementation

--- ubuntu-distro-info/src/main.rs
+++ ubuntu-distro-info/src/main.rs
@@ -1,3 +1,10 @@
+extern crate distro_info;
+
+use distro_info::UbuntuDistroInfo;
+
 fn main() {
-    println!("Hello, world!");
+    let ubuntu_distro_info = UbuntuDistroInfo::new().unwrap();
+    for distro_release in ubuntu_distro_info {
+        println!("{}", distro_release.series);
+    }
 }
