246fddf5d1d4d0b8431d58034a24a8d48aa3019f
Daniel Watkins <daniel.watkins@canonical.com>
2018-06-23 22:36:58 -0400

    feat(udi): add help text for options

--- ubuntu-distro-info/src/main.rs
+++ ubuntu-distro-info/src/main.rs
@@ -87,25 +87,79 @@ fn run() -> Result<(), Error> {
     let matches = App::new("ubuntu-distro-info")
         .version("0.1.0")
         .author("Daniel Watkins <daniel@daniel-watkins.co.uk>")
-        .arg(Arg::with_name("all").short("a").long("all"))
-        .arg(Arg::with_name("devel").short("d").long("devel"))
+        .arg(
+            Arg::with_name("all")
+                .short("a")
+                .long("all")
+                .help("list all known versions"),
+        )
+        .arg(
+            Arg::with_name("devel")
+                .short("d")
+                .long("devel")
+                .help("latest development version"),
+        )
         .arg(Arg::with_name("latest").short("l").long("latest"))
-        .arg(Arg::with_name("lts").long("lts"))
-        .arg(Arg::with_name("series").long("series").takes_value(true))
-        .arg(Arg::with_name("stable").short("s").long("stable"))
-        .arg(Arg::with_name("supported").long("supported"))
-        .arg(Arg::with_name("unsupported").long("unsupported"))
-        .arg(Arg::with_name("codename").short("c").long("codename"))
-        .arg(Arg::with_name("fullname").short("f").long("fullname"))
-        .arg(Arg::with_name("release").short("r").long("release"))
-        .arg(Arg::with_name("date").long("date").takes_value(true))
+        .arg(
+            Arg::with_name("lts")
+                .long("lts")
+                .help("latest long term support (LTS) version"),
+        )
+        .arg(
+            Arg::with_name("series")
+                .long("series")
+                .takes_value(true)
+                .help("series to calculate the version for"),
+        )
+        .arg(
+            Arg::with_name("stable")
+                .short("s")
+                .long("stable")
+                .help("latest stable version"),
+        )
+        .arg(
+            Arg::with_name("supported")
+                .long("supported")
+                .help("list of all supported stable versions"),
+        )
+        .arg(
+            Arg::with_name("unsupported")
+                .long("unsupported")
+                .help("list of all unsupported stable versions"),
+        )
+        .arg(
+            Arg::with_name("codename")
+                .short("c")
+                .long("codename")
+                .help("print the codename (default)"),
+        )
+        .arg(
+            Arg::with_name("fullname")
+                .short("f")
+                .long("fullname")
+                .help("print the full name"),
+        )
+        .arg(
+            Arg::with_name("release")
+                .short("r")
+                .long("release")
+                .help("print the release version"),
+        )
+        .arg(
+            Arg::with_name("date")
+                .long("date")
+                .takes_value(true)
+                .help("date for calculating the version (default: today)"),
+        )
         .arg(
             Arg::with_name("days")
                 .short("y")
                 .long("days")
                 .takes_value(true)
                 .default_value("release")
-                .possible_values(&["created", "eol", "eol-server", "release"]),
+                .possible_values(&["created", "eol", "eol-server", "release"])
+                .value_name("milestone")
+                .help("additionally, display days until milestone"),
         )
         .group(
             ArgGroup::with_name("selector")
