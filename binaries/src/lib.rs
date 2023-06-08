a9d373c7aa41a303d771319cfb0c51991dd0cdba
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-06-08 13:27:23 -0400

    binaries: handle no-version correctly for OutputMode::Release

--- binaries/src/lib.rs
+++ binaries/src/lib.rs
@@ -188,7 +188,14 @@ pub fn output(
         let mut output_parts = vec![];
         match output_mode {
             OutputMode::Codename => output_parts.push(distro_release.series().to_string()),
-            OutputMode::Release => output_parts.push(distro_release.version().to_string()),
+            OutputMode::Release => output_parts.push(
+                if distro_release.version() == "" {
+                    distro_release.series()
+                } else {
+                    distro_release.version()
+                }
+                .to_string(),
+            ),
             OutputMode::FullName => output_parts.push(format!(
                 "{} {} \"{}\"",
                 distro_name,
