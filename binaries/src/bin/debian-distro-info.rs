a1a4d41b07ea8367dc7b6108792f2cf5a52ce9a5
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-06-08 11:42:24 -0400

    binaries: introduce initial debian-distro-info implementation

---
+++ binaries/src/bin/debian-distro-info.rs
@@ -0,0 +1,20 @@
+use clap::{App, Arg};
+use distro_info::{DebianDistroInfo, DistroInfo};
+use distro_info_binaries::{add_common_args, common_run};
+use failure::Error;
+
+fn run() -> Result<(), Error> {
+    let app = add_common_args(App::new("debian-distro-info"), &[]);
+    let matches = app.get_matches();
+    let debian_distro_info = DebianDistroInfo::new()?;
+    common_run(&matches, &debian_distro_info)
+}
+
+fn main() {
+    if let Err(ref e) = run() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        writeln!(stderr, "debian-distro-info: {}", e).unwrap();
+        ::std::process::exit(1);
+    }
+}
