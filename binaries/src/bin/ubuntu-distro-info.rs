c6abc7f8d612237f47c3577650b6bd452753f700
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-06-19 14:41:24 -0400

    binaries: move common matching code into lib

--- binaries/src/bin/ubuntu-distro-info.rs
+++ binaries/src/bin/ubuntu-distro-info.rs
@@ -9,12 +9,11 @@ use distro_info_binaries::{add_common_args, common_run, flag};
 
 fn run() -> Result<(), Error> {
     let additional_selectors = &["latest", "lts"];
-    let app = add_common_args("ubuntu-distro-info", additional_selectors)
+    let command = add_common_args("ubuntu-distro-info", additional_selectors)
         .arg(flag("latest", Some('l'), ""))
         .arg(flag("lts", None, "latest long term support (LTS) version"));
-    let matches = app.try_get_matches()?;
     let ubuntu_distro_info = UbuntuDistroInfo::new()?;
-    common_run(&matches, &ubuntu_distro_info)
+    common_run(command, &ubuntu_distro_info)
 }
 
 fn main() {
