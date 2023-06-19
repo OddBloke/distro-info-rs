039ff2a0ffd5e8252b8c0b8568d418b0da8633bc
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-06-19 14:30:14 -0400

    binaries: add flag helper function

--- binaries/src/bin/debian-distro-info.rs
+++ binaries/src/bin/debian-distro-info.rs
@@ -1,16 +1,14 @@
 use anyhow::Error;
-use clap::{Arg, ArgAction, Command};
+use clap::Command;
 use distro_info::{DebianDistroInfo, DistroInfo};
-use distro_info_binaries::{add_common_args, common_run};
+use distro_info_binaries::{add_common_args, common_run, flag};
 
 fn run() -> Result<(), Error> {
-    let app = add_common_args(Command::new("debian-distro-info"), &["testing"]).arg(
-        Arg::new("testing")
-            .action(ArgAction::SetTrue)
-            .short('t')
-            .long("testing")
-            .help("current testing version"),
-    );
+    let app = add_common_args(Command::new("debian-distro-info"), &["testing"]).arg(flag(
+        "testing",
+        't',
+        "current testing version",
+    ));
     let matches = app.try_get_matches()?;
     let debian_distro_info = DebianDistroInfo::new()?;
     common_run(&matches, &debian_distro_info)
