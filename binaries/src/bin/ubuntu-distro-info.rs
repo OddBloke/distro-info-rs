02838486a4626c6c84cb8759c93f31e41d827c31
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-06-20 11:58:03 -0400

    binaries: refactor common main() into lib

--- binaries/src/bin/ubuntu-distro-info.rs
+++ binaries/src/bin/ubuntu-distro-info.rs
@@ -9,24 +9,18 @@ use anyhow::Error;
 use distro_info::{DistroInfo, UbuntuDistroInfo};
 use distro_info_binaries::{common_run, DistroInfoCommand};
 
-fn run() -> Result<(), Error> {
+fn run(command: DistroInfoCommand) -> Result<(), Error> {
+    let ubuntu_distro_info = UbuntuDistroInfo::new()?;
+    common_run(command, &ubuntu_distro_info)
+}
+
+fn main() {
     let command = DistroInfoCommand {
         command_name: "ubuntu-distro-info",
         additional_selectors: HashMap::from([
             ("latest", (Some('l'), "")),
             ("lts", (None, "latest long term support (LTS) version")),
         ]),
-    }
-    .create_command();
-    let ubuntu_distro_info = UbuntuDistroInfo::new()?;
-    common_run(command, &ubuntu_distro_info)
-}
-
-fn main() {
-    if let Err(ref e) = run() {
-        use std::io::Write;
-        let stderr = &mut ::std::io::stderr();
-        writeln!(stderr, "ubuntu-distro-info: {}", e).unwrap();
-        ::std::process::exit(1);
-    }
+    };
+    command.main(&run)
 }
